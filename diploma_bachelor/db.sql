af77f719bf7e5447fa47cf7dcae0149d2434a668
Nadezhda <55202774+ClavicusVile98@users.noreply.github.com>
2020-08-20 04:15:12 +0300

    Add files via upload
    Required files for system. Detailed description in readme.md

---
+++ diploma_bachelor/db.sql
@@ -0,0 +1,5 @@
+INSERT INTO logs (flowtime, watertype) VALUES (FROM_UNIXTIME(1543344999),'hot');
+INSERT INTO logs (flowtime, watertype) VALUES (FROM_UNIXTIME(1543350980),'cold');
+INSERT INTO logs (flowtime, watertype) VALUES (FROM_UNIXTIME(1543351113),'hot');
+INSERT INTO logs (flowtime, watertype) VALUES (FROM_UNIXTIME(1543351244),'cold');
+INSERT INTO logs (flowtime, watertype) VALUES (FROM_UNIXTIME(1543351256),'hot');
