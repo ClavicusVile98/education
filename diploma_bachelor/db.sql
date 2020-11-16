f859779d940aabfa616064b6dc6fb71776ca9aae
Nadezhda <55202774+ClavicusVile98@users.noreply.github.com>
2020-11-17 01:30:10 +0300

    del diploma

--- diploma_bachelor/db.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO logs (flowtime, watertype) VALUES (FROM_UNIXTIME(1543344999),'hot');
-INSERT INTO logs (flowtime, watertype) VALUES (FROM_UNIXTIME(1543350980),'cold');
-INSERT INTO logs (flowtime, watertype) VALUES (FROM_UNIXTIME(1543351113),'hot');
-INSERT INTO logs (flowtime, watertype) VALUES (FROM_UNIXTIME(1543351244),'cold');
-INSERT INTO logs (flowtime, watertype) VALUES (FROM_UNIXTIME(1543351256),'hot');
